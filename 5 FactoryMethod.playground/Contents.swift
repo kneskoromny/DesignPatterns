aae33513a0aa5360b030139ad333efb1a64a0818
kneskoromny <kneskoromny@gmail.com>
2021-10-02 16:12:01 +0300

    factory method

---
+++ 5 FactoryMethod.playground/Contents.swift
@@ -0,0 +1,54 @@
+// протокол для транспорт ср-ва
+protocol Vehicle {
+    func drive()
+}
+class Car: Vehicle {
+    func drive() {
+        print("drive a car")
+    }
+}
+class Truck: Vehicle {
+    func drive() {
+        print("drive a truck")
+    }
+}
+class Bus: Vehicle {
+    func drive() {
+        print("drive a bus")
+    }
+}
+// протокол для фабрики
+protocol VehicleFactory {
+    func produce() -> Vehicle
+}
+
+class CarFactory: VehicleFactory {
+    func produce() -> Vehicle {
+        print("car is created")
+        return Car()
+    }
+}
+class TruckFactory: VehicleFactory {
+    func produce() -> Vehicle {
+        print("truck is created")
+        return Truck()
+    }
+}
+class  BusFactory: VehicleFactory {
+    func produce() -> Vehicle {
+        print("bus is created")
+        return Bus()
+    }
+}
+
+let carFacory = CarFactory()
+let car = carFacory.produce()
+
+let truckFactory = TruckFactory()
+let truck = truckFactory.produce()
+
+let busFactory = BusFactory()
+let bus = busFactory.produce()
+
+
+
