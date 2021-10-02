3d53c26f40c4c7fcabfc5670b3e87dad7b936403
kneskoromny <kneskoromny@gmail.com>
2021-10-02 16:29:20 +0300

    abstract factory

---
+++ 6 AbstractFactory.playground/Contents.swift
@@ -0,0 +1,71 @@
+
+
+// протокол для авто
+protocol Car {
+    func drive()
+}
+class LittleSizeCar: Car {
+    func drive() {
+        print("drive a little car")
+    }
+}
+class MiddleSizeCar: Car {
+    func drive() {
+        print("drive a middle car")
+    }
+}
+
+protocol Bus {
+    func drive()
+}
+class LittleSizeBus: Bus {
+    func drive() {
+        print("drive a little bus")
+    }
+}
+class MiddleSizeBus: Bus {
+    func drive() {
+        print("drive a middle size bus")
+    }
+}
+
+
+protocol Factory {
+    // создает и авто и бус
+    func produceBus() -> Bus
+    func produceCar() -> Car
+}
+class LittleSizeFactory: Factory {
+    func produceBus() -> Bus {
+        print("Little bus is created")
+        return LittleSizeBus()
+    }
+    
+    func produceCar() -> Car {
+        print("Little size car is creates")
+        return LittleSizeCar()
+    }
+}
+class MiddleSizeFactory: Factory {
+    func produceBus() -> Bus {
+        print("Middle bus is created")
+        return MiddleSizeBus()
+    }
+    
+    func produceCar() -> Car {
+        print("Middle size car is creates")
+        return MiddleSizeCar()
+    }
+}
+
+let littleFactory = LittleSizeFactory()
+littleFactory.produceCar()
+
+
+let middleFactory = MiddleSizeFactory()
+middleFactory.produceBus()
+
+
+
+
+
