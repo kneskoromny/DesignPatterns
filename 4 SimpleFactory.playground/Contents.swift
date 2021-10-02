f82f0910e672598d07e0c4c93da54687f5181c52
kneskoromny <kneskoromny@gmail.com>
2021-10-02 16:07:05 +0300

    simple factory

--- 4 SimpleFactory.playground/Contents.swift
+++ 4 SimpleFactory.playground/Contents.swift
@@ -7,34 +7,25 @@ enum CarType {
 }
 
 protocol Car {
-    
     func drive()
 }
-
 class HugeCar: Car {
-    
     func drive() {
-        
         print("you drive huge car")
     }
 }
-
 class FastCar: Car {
-    
     func drive() {
-        
         print("you drive fast car")
     }
 }
-
 let hugeCar = HugeCar()
 hugeCar.drive()
-
 let fastCar = FastCar()
 fastCar.drive()
 
+// фабрика
 class CarFactory {
-    
     static func produceCar(type: CarType) -> Car {
         var car: Car
         
@@ -42,12 +33,9 @@ class CarFactory {
         case .fast: car = FastCar()
         case .huge: car = HugeCar()
         }
-        
         return car
     }
 }
-
-
 let hugeCar1 = CarFactory.produceCar(type: .huge)
 let fastCar1 = CarFactory.produceCar(type: .fast)
 
